a796bb1c62b5f553cf3ce7eda647b27bf9f1f394
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-06-01 10:18:50 -0400

    added JS to toggle read button styling

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # sign-up-form
+- Focusing on clean, modular CSS with limited extra rules.
--- library.js
+++ library.js
@@ -1,18 +1,28 @@
 // DOM Access
-const read = document.getElementById("btn-read");
+const read = document.querySelectorAll("#btn-read");
+const ele = document.querySelector(":root");
+const redColor = getComputedStyle(ele).getPropertyValue("--red");
+const greenColor = getComputedStyle(ele).getPropertyValue("--green");
 let readStatus = true;
 
 // functions
-function toggleRead() {
-  if (read) {
-    read.style.backgroundColor = "rgb(98, 228, 65)";
+function toggleRead(reads) {
+  if (readStatus) {
+    reads.classList.remove("not-read");
+    reads.classList.add("read");
+    reads.innerHTML = "READ";
   } else {
-    read.style.backgroundColor = "rgb(249, 55, 55)";
+    reads.classList.remove("read");
+    reads.classList.add("not-read");
+    reads.innerHTML = "NOT READ";
   }
+  console.log(readStatus);
 }
 
 // Event Listeners
-read.addEventListener("change", (e) => {
-  readStatus = !readStatus;
-  toggleRead();
-});
+read.forEach((reads) =>
+  reads.addEventListener("click", (e) => {
+    toggleRead(reads);
+    readStatus = !readStatus;
+  })
+);
